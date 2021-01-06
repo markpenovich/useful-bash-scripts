475a9875ed347f540e934119c39109b5fedd1068
Mark Penovich <mpenovich1@gmail.com>
2021-01-06 16:25:39 -0600

    Comment and clean up code

--- backup.sh
+++ backup.sh
@@ -7,20 +7,27 @@ timestamp=$(date +"%Y-%m-%dT%H:%M:%S%z")
 
 #Folders
 dest=~/backups
-sauce=~/Pictures
+sauce=~/Pictures #Change this folder to whichever directory you would like to backup
 
+# Check if the backup folder exists in the home directory, if not create one
 if [ -d $dest ]; then
-echo "Backup folder exits"
+  echo "Backup folder exits"
 else
-echo "Backup folder does not exist! I'm now creating it..."; (mkdir -p $dest)
-echo
+  echo "Backup folder does not exist! I'm now creating it..."; (mkdir -p $dest)
+  echo
 fi
-read -p "Press any key to continue..." -n1 -s
-mkdir -p $dest/$timestamp
 
+read -p "Press any key to continue..." -n1 -s
+
+# Make new directory inside backups folder with timestamp of backup, clear screen, inform user backup is taking place
+mkdir -p $dest/$timestamp
 clear
 echo "Now backing up. Please wait..."
-tar cvfz $dest/$timestamp/pictures-backup.tgz $sauce
+
+# Create a compressed tar file in backups/timestamp 
+tar cvfz $dest/$timestamp/backup.tgz $sauce
+
+# Clear screen, inform user the backup has been completed
 clear 
 echo "Backup complete. Go to the /backups folder in order to see your compressed file."
 
